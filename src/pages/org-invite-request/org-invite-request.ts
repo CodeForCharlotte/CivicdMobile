f5cbb17df26ea5a75ad290e4abc90641275ac858
john sander <jrjackets112@gmail.com>
2018-04-26 14:04:12 -0400

    Update org invite and event forms to match DB and complete tag search

--- src/pages/org-invite-request/org-invite-request.ts
+++ src/pages/org-invite-request/org-invite-request.ts
@@ -19,10 +19,11 @@ export class OrgInviteRequestPage {
 
   submitOrgInfo(form) {
     var orgInfo = form.value;
+
     console.log(orgInfo);
     let alert = this.alertCtrl.create({
       title: "Thank You!",
-      subTitle: "We received your request and will follow up with you at " + form.value.orgEmail + " once we review your information.",
+      subTitle: "We received your request and will follow up with you at " + form.value.Email + " once we review your information.",
       buttons: [{
         text: "OK",
         handler: () => {
